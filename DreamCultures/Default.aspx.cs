7f8b7a74f7014da8bd16409e9ba2df34b45882bf
Tarluna <tarluna@gmail.com>
2017-02-27 09:41:22 -0300

    Add source code

---
+++ DreamCultures/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DreamCultures
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DatabaseLayerDataContext t = new DatabaseLayerDataContext();
+            var asd = t.Users.Select(c => c.FirstName == "Fermamdp");
+            t.Users.Any(c => c.FirstName.Contains("Fernando"));
+
+            var asddsfasdf = new { name = "tarcisio"};
+            int luna = 10000;
+            string name = string.Format($"{asddsfasdf.name} {luna}");
+
+            var tsdfg = "fdghdfgh";
+            Dreaming dreaming = new Dreaming();
+            List<Dream> dreams = dreaming.GetDreamsByUser();
+            dreams.Where(p => p.QuarterID == 0);
+            foreach (Dream item in dreams)
+            {
+
+            }
+        }
+
+        private void method123(object asddsfasdf)
+        {
+            
+        }
+    }
+}
---
+++ DreamCultures/Dreaming.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DreamCultures
+{
+    public class Dreaming
+    {
+        int userID = 0;
+        int quarter = 0;
+        DatabaseLayerDataContext context = new DatabaseLayerDataContext();
+        
+        public List<Dream> GetDreamsByUser()
+        {
+            return context.Dreams.Where(c => c.UserID == userID).ToList();
+        }
+    }
+}
